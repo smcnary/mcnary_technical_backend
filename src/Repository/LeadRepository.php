a024cccc453b51e8fb2f3af603eb3121d39261ee
Sean McNary <seanmcnary@attlocal.net>
2025-08-11 17:12:17 -0500

    feat: Add repository classes for new entities
    - LeadRepository: Extends ServiceEntityRepository with save/remove methods
    - CaseStudyRepository: Extends ServiceEntityRepository with save/remove methods
    - FaqRepository: Extends ServiceEntityRepository with save/remove methods
    - All repositories follow Doctrine best practices and include proper PHPDoc

---
+++ src/Repository/LeadRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Lead;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Lead>
+ *
+ * @method Lead|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Lead|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Lead[]    findAll()
+ * @method Lead[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class LeadRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Lead::class);
+    }
+
+    public function save(Lead $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Lead $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
