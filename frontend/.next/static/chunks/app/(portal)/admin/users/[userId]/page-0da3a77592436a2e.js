e624854f3450bc31f020f68091a6018c11dc90c8
Sean McNary <seanmcnary@attlocal.net>
2025-08-19 07:33:43 -0500

    frontend: remove Vite artifacts (index.html, vite config, SPA entry); migrate styles

---
+++ frontend/.next/static/chunks/app/(portal)/admin/users/[userId]/page-0da3a77592436a2e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[169,255,749,921,695,773,873,860,835,28,446,846,409,953,655,98,287],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
---
+++ frontend/.next/static/chunks/app/(portal)/client/page-93ec180249591074.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[953,255,749,921,695,773,873,169,860,835,28,446,846,409,655,98,287],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
